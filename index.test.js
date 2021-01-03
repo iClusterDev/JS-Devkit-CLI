81fd3e7cdbf941aadd469140c200aa9944d8946a
iClusterDev <cluster.coredev@gmail.com>
2021-01-03 14:26:37 +0000

    cleanup

--- index.test.js
+++ index.test.js
@@ -1,10 +1,7 @@
 const create = require('./lib/Create');
-
 console.log('starting test!');
-
 // test for lib folder exists
 // test for template folder exists
-
 if (!create) {
   throw new Error('unable to get create from ./lib');
 } else {
